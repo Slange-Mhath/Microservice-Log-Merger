4be580207521e605e4902832e867a8d40e20e48d
WebSepp <bastilange@t-online.de>
2022-02-11 18:53:38 +0000

    Change pdf merging
    This changes how the pdf merging works to apply with the new
    changes from the pdf_analyser.
    Might need more tests

--- merge_analysed_pdfs.py
+++ merge_analysed_pdfs.py
@@ -11,7 +11,7 @@ def add_pdf_info_to_db(pdf_analyser_log_path, session, File):
             if session.query(File).filter(
                     File.path == pdf[0]).count() > 0:
                 session.query(File).filter(File.path == pdf[0]).update({File.pdf_info: json.dumps(
-                            create_pdf_info(pdf[1]))},
+                           pdf[1])},
                         synchronize_session=False)
                 pdf_counter += 1
                 if num % 1000 == 0:
@@ -22,10 +22,10 @@ def add_pdf_info_to_db(pdf_analyser_log_path, session, File):
         print("{} pdf info uploaded into the DB".format(pdf_counter))
 
 
-def create_pdf_info(raw_pdf_info):
-    formatted_pdf_info = {"pdf_tool_version": raw_pdf_info["tool_version_info"]}
-    if raw_pdf_info["isText"] is False:
-        formatted_pdf_info["pdf_is_image"] = True
-    else:
-        formatted_pdf_info["pdf_is_image"] = False
-    return formatted_pdf_info
+# def create_pdf_info(raw_pdf_info):
+#     formatted_pdf_info = {"pdf_tool_version": raw_pdf_info["tool_version_info"]}
+#     if raw_pdf_info["isText"] is False:
+#         formatted_pdf_info["pdf_is_image"] = True
+#     else:
+#         formatted_pdf_info["pdf_is_image"] = False
+#     return formatted_pdf_info
