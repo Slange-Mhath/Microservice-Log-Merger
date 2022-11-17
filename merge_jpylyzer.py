94f465fb48a1159b252495b3274b0f5921d6efe7
WebSepp <bastilange@t-online.de>
2022-11-17 11:32:27 +0000

    DEV: Adjust to new jpylyzer output

--- merge_jpylyzer.py
+++ merge_jpylyzer.py
@@ -7,12 +7,17 @@ def add_jpylyzer_info_to_db(jp_f_log_path, session, File):
         jp_json = json.load(jp_log)
         jp_counter = 0
         # Here needs to go an exception if jp_json has no jpylyzer and then no file key
-        for num, jp_f in enumerate(jp_json['jpylyzer']):
-            if session.query(File).filter(File.path == jp_f['file']['fileInfo'][
+        for num, jp_f in enumerate(jp_json['jpylyzer']['file']):
+            if num < 2:
+                print(jp_f)
+            # Todo: it has to iterate over file
+            # Todo: which then would mean we need to change that to only be ['fileInfo'][
+            #                 'filePath']
+            if session.query(File).filter(File.path == jp_f['fileInfo'][
                 'filePath']).count() > 0:
                 jp_counter += 1
                 session.query(File).filter(
-                    File.path == jp_f['file']['fileInfo']['filePath']).update(
+                    File.path == jp_f['fileInfo']['filePath']).update(
                     {File.jpylyzer_file_info: json.dumps(
                         relabel_jpylyzer_log(jp_f))},
                     synchronize_session=False)
@@ -26,41 +31,41 @@ def add_jpylyzer_info_to_db(jp_f_log_path, session, File):
 def relabel_jpylyzer_log(jp_f):
     relabled_log = {}
     try:
-        relabled_log['is_valid'] = jp_f['file']['isValid']
+        relabled_log['is_valid'] = jp_f['isValid']
     except KeyError:
         logging.warning(
-            f" {jp_f['file']['fileInfo']['filePath']} has no Key 'isValid'")
+            f" {jp_f['fileInfo']['filePath']} has no Key 'isValid'")
     try:
         relabled_log['tool_info'] = jp_f['toolInfo']
     except KeyError:
         logging.warning(f" {jp_f['fileInfo']['filePath']} has no Key toolInfo")
-    if jp_f['file']['properties']:
-        if 'contiguousCodestreamBox' in jp_f['file']['properties']:
-            if 'com' in jp_f['file']['properties']['contiguousCodestreamBox']:
+    if jp_f['properties']:
+        if 'contiguousCodestreamBox' in jp_f['properties']:
+            if 'com' in jp_f['properties']['contiguousCodestreamBox']:
                 try:
                     relabled_log['properties'] = {"contiguousCodestreamBox": {
-                        "com": jp_f['file']['properties'][
+                        "com": jp_f['properties'][
                             'contiguousCodestreamBox']['com']}}
                 except KeyError:
                     logging.warning(
-                        f" {jp_f['file']['fileInfo']['filePath']} has no Key 'com'")
-            if 'cod' in jp_f['file']['properties']['contiguousCodestreamBox']:
+                        f" {jp_f['fileInfo']['filePath']} has no Key 'com'")
+            if 'cod' in jp_f['properties']['contiguousCodestreamBox']:
                 try:
                     relabled_log['properties']["contiguousCodestreamBox"][
-                        'cod'] = {"order": jp_f['file']['properties'][
+                        'cod'] = {"order": jp_f['properties'][
                         'contiguousCodestreamBox']['cod']['order']}
                     relabled_log['properties'][
                         "contiguousCodestreamBox"]['cod']['sop'] = \
-                    jp_f['file']['properties']['contiguousCodestreamBox'][
+                    jp_f['properties']['contiguousCodestreamBox'][
                         'cod']['sop']
                     relabled_log['properties'][
-                        "contiguousCodestreamBox"]["cod"]["eph"] = jp_f["file"]['properties']['contiguousCodestreamBox']['cod']['eph']
-                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["codeBlockWidth"] = jp_f['file']['properties']['contiguousCodestreamBox']['cod']['codeBlockWidth']
-                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["codeBlockHeight"] = jp_f['file']['properties']['contiguousCodestreamBox']['cod']['codeBlockHeight']
-                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["transformation"] = jp_f['file']['properties']['contiguousCodestreamBox']['cod']['transformation']
-                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["layers"] = jp_f['file']['properties']['contiguousCodestreamBox']['cod']['layers']
-                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["levels"] = jp_f['file']['properties']['contiguousCodestreamBox']['cod']['levels']
+                        "contiguousCodestreamBox"]["cod"]["eph"] = jp_f['properties']['contiguousCodestreamBox']['cod']['eph']
+                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["codeBlockWidth"] = jp_f['properties']['contiguousCodestreamBox']['cod']['codeBlockWidth']
+                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["codeBlockHeight"] = jp_f['properties']['contiguousCodestreamBox']['cod']['codeBlockHeight']
+                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["transformation"] = jp_f['properties']['contiguousCodestreamBox']['cod']['transformation']
+                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["layers"] = jp_f['properties']['contiguousCodestreamBox']['cod']['layers']
+                    relabled_log['properties']["contiguousCodestreamBox"]["cod"]["levels"] = jp_f['properties']['contiguousCodestreamBox']['cod']['levels']
                 except KeyError:
                     logging.warning(
-                        f" {jp_f['file']['fileInfo']['filePath']} required Key is not in log")
+                        f" {jp_f['fileInfo']['filePath']} required Key is not in log")
     return relabled_log
